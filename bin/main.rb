4ebeff9412604c34b59a2685e842bf479c11b4ae
maosan132 <maosan132@gmail.com>
2020-06-02 17:27:16 -0500

    move board class to its own file under lib folde

--- bin/main.rb
+++ bin/main.rb
@@ -5,74 +5,6 @@
 
 # require_relative '../lib/board.rb'
 
-class Board
-  attr_reader :board
-
-  def initialize
-
-    # @board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
-    # @board = %W(\s \s \s \s \s \s \s \s \s)
-    play_again
-  end
-
-  def play_again
-    @board = Array.new(9, '-')
-  end
-  def display
-    puts
-    puts ' ' + board[0] + ' │ ' + board[1] + ' │ ' + board[2] + '    ░   1   2   3'
-    puts '———┼———┼———' + (' ' * 3) + '░'
-    puts ' ' + board[3] + ' │ ' + board[4] + ' │ ' + board[5] + '    ░   4   5   6'
-    puts '———┼———┼———' + (' ' * 3) + '░'
-    puts ' ' + board[6] + ' │ ' + board[7] + ' │ ' + board[8] + '    ░   7   8   9'
-    puts
-  end
-
-  def update(choice, current_player)
-    board[choice - 1] = current_player
-
-  end
-
-  def free_cells
-
-  end
-
-  def winner?(current_player)
-    if board[0] == current_player && board[1] == current_player && board[2] == current_player
-      true
-    elsif board[3] == current_player && board[4] == current_player && board[5] == current_player
-      true
-    elsif board[6] == current_player && board[7] == current_player && board[8] == current_player
-      true
-    elsif board[0] == current_player && board[3] == current_player && board[6] == current_player
-      true
-    elsif board[1] == current_player && board[4] == current_player && board[7] == current_player
-      true
-    elsif board[2] == current_player && board[5] == current_player && board[8] == current_player
-      true
-    elsif board[0] == current_player && board[4] == current_player && board[8] == current_player
-      true
-    elsif board[6] == current_player && board[4] == current_player && board[2] == current_player
-      true
-    else
-      false
-    end
-  end
-
-  def valid(choice)
-    (1..9).include?(choice)
-  end
-
-  def taken?(choice)
-    # board.include?(choice)
-    board[choice - 1] != '-'
-  end
-
-  def tie?
-    !board.include?('-')
-  end
-end 
-
 class Player
   attr_reader :name
 
@@ -157,7 +89,7 @@ while game_on
     presentation
     puts "and the winner is #{turn}"
 
-    break
+    
   end
 
 
