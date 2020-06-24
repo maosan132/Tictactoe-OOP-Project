81ec1de8c51947ff7364596b31ca54bf3a7b3db3
maosan132 <maosan132@gmail.com>
2020-06-24 14:57:32 -0500

    refactored horiz, vert and diag winner methods for code cleanliness

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,9 @@
 require './lib/board'
+let (:turn_x) { 'X' }
+let ()
 
 describe Board do
-  let(:board) { Board.new }
+  let(:gameboard) { Board.new }
   describe '#' do
     it 'should' do # for sake
       expect(21).to eq(21)
@@ -11,7 +13,7 @@ describe Board do
   describe '#display' do
     it 'should output a grid for the board' do
       alias_method :draw, :puts
-      expect(board.display).to eq(print("  ┌———┬———┬———┐     ░
+      expect(gameboard.display).to eq(print("  ┌———┬———┬———┐     ░
         | - | - │ - │     ░   1   2   3
         ├———┼———┼———┤     ░
         | - | - │ - │     ░   4   5   6
@@ -23,7 +25,9 @@ describe Board do
 
   describe '#horizontal_winner?(player)' do
     it 'should return true when all three board[] positions are the same' do
-      expect(21).to eq(21)
+      gameboard.board[0], gameboard.board[1], board[2] = 3
+
+      expect(gameboard.horizontal_winner?('X')).to eq(21)
     end
     it 'should return false when not horizontal winner' do
       expect(21).to eq(21)
