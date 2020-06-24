eea004767f6466d40c49f0b2f5a70ed4ac070832
maosan132 <maosan132@gmail.com>
2020-06-24 15:59:52 -0500

    added two more test: vert and diag winner, similar than horiz winner

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -34,4 +34,34 @@ describe Board do
       expect(gameboard.horizontal_winner?('X')).to eq(false)
     end
   end
+
+  describe '#vertical_winner?(player)' do
+    it 'should return true when get a vertical line of symbol X' do
+      gameboard.update(1, 'X')
+      gameboard.update(4, 'X')
+      gameboard.update(7, 'X')
+      expect(gameboard.vertical_winner?('X')).to eq(true)
+    end
+    it 'should return false when not get a vertical line of symbol X' do
+      gameboard.update(1, 'Y')
+      gameboard.update(4, 'X')
+      gameboard.update(7, 'Y')
+      # binding.pry
+      expect(gameboard.vertical_winner?('X')).to eq(false)
+    end
+  end
+
+  describe '#diagonal_winner?(player)' do
+    it 'should return true when get a diagonal line of symbol X' do
+      gameboard.update(0, 'X')
+      gameboard.update(4, 'X')
+      expect(gameboard.diagonal_winner?('X')).to eq(true)
+    end
+    it 'should return false when not get a diagonal line of symbol X' do
+      gameboard.update(0, 'Y')
+      gameboard.update(4, 'X')
+      # binding.pry
+      expect(gameboard.diagonal_winner?('X')).to eq(false)
+    end
+  end
 end
